3360d386a51dcfe7ad702160675db0cf684bae84
shobhitsaini <80474021+shobhit007@users.noreply.github.com>
2025-01-21 23:13:21 +0530

    fixed follow update issue

--- admin/sales/salesPanel.js
+++ admin/sales/salesPanel.js
@@ -26,10 +26,11 @@ const updateLead = async (req, res) => {
   try {
     const body = req.body;
     const leadId = body.leadId;
-    const followUpDate = body.followUpDate
-      ? Timestamp.fromDate(moment(body.followUpDate).toDate())
+    const followUpDate = body.followUpDate;
+    const updatedFollowUpdate = followUpDate
+      ? moment(followUpDate).utc().toDate()
       : null;
-    body.followUpDate = followUpDate;
+    body.followUpDate = updatedFollowUpdate;
     delete body.leadId;
 
     const leadRef = db.collection("leads").doc(`1click${leadId}`);
